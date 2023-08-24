342b10e644b3ac76e5b006af33a55ff233117e3a
joni31reus <hechtjonas@web.de>
2023-08-24 15:08:39 +0200

    Update SAC_TestingDateTimePicker.js

--- SAC_TestingDateTimePicker.js
+++ SAC_TestingDateTimePicker.js
@@ -1,44 +1,147 @@
 (function(){
 
-    let shadowRoot,
+    let _shadowRoot,
+        _id,
         template = document.createElement("template");
-    template.innerHTML = `<style></style>`;
+    template.innerHTML = `
+                            <style></style>
+                            <div id="ui5_content name="ui5_content>
+                                <slot name="content"></slot>
+                            </div>
+
+                            <script id="oView" name="oView" type="sapui5/xmlview">
+                                <mvc:View
+                                    controllerName="myView.Template"
+                                    xmlns:l="sap.ui.layout"
+                                    xmlns:mvc="sap.ui.core.mvc"
+                                    xmlns="sap.m">
+                                    <l:VerticalLayout
+                                        class="sapUiContentPadding"
+                                        width="100%">
+                                            <l:content>
+                                                <DateTimePicker
+                                                    id="DTP_1"
+                                                    placeholder="Enter date..."/>
+                                            </l:content>
+                                    </l:VerticalLayout>
+                                </mvc:View>
+                            </script>
+                        `;
                         
     class TestingDateTimePicker extends HTMLElement{
         constructor(){
             super();
 
-            shadowRoot = this.attachShadow({mode: "open"});
-            shadowRoot.appendChild(template.content.cloneNode(true));
+            _shadowRoot = this.attachShadow({mode: "open"});
+            _shadowRoot.appendChild(template.content.cloneNode(true));
 
-            loadDateTimePicker();
+            _id = createGuid();
+
+            _shadowRoot.querySelector("#oView").id = _id + "_oView";
         }
 
-        onCustomWidgetBeforeUpdate(oChangedProperties){
-            console.log("onCustomWidgetBeforeUpdate");
-            console.log(oChangedProperties);
+        connectedCallback() {
+            try {
+                if (window.commonApp) {
+                    let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
+
+                    if (outlineContainer && outlineContainer.getReactProps) {
+                        let parseReactState = state => {
+                            let components = {};
+
+                            let globalState = state.globalState;
+                            let instances = globalState.instances;
+                            let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
+                            let names = app.names;
+
+                            for (let key in names) {
+                                let name = names[key];
+
+                                let obj = JSON.parse(key).pop();
+                                let type = Object.keys(obj)[0];
+                                let id = obj[type];
+
+                                components[id] = {
+                                    type: type,
+                                    name: name
+                                };
+                            }
+
+                            for (let componentId in components) {
+                                let component = components[componentId];
+                            }
+
+                            let metadata = JSON.stringify({
+                                components: components,
+                                vars: app.globalVars
+                            });
+
+                            if (metadata != this.metadata) {
+                                this.metadata = metadata;
+
+                                this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                                    detail: {
+                                        properties: {
+                                            metadata: metadata
+                                        }
+                                    }
+                                }));
+                            }
+                        };
+
+                        let subscribeReactStore = store => {
+                            this._subscription = store.subscribe({
+                                effect: state => {
+                                    parseReactState(state);
+                                    return {
+                                        result: 1
+                                    };
+                                }
+                            });
+                        };
+
+                        let props = outlineContainer.getReactProps();
+                        if (props) {
+                            subscribeReactStore(props.store);
+                        } else {
+                            let oldRenderReactComponent = outlineContainer.renderReactComponent;
+                            outlineContainer.renderReactComponent = e => {
+                                let props = outlineContainer.getReactProps();
+                                subscribeReactStore(props.store);
+
+                                oldRenderReactComponent.call(outlineContainer, e);
+                            }
+                        }
+                    }
+                }
+            } catch (e) {}
         }
 
-        onCustomWidgetAfterUpdate(oChangedProperties){
-            console.log("onCustomWidgetAfterUpdate");
-            console.log(oChangedProperties);
+        disconnectedCallback() {
+            if (this._subscription) { 
+                this._subscription();
+                this._subscription = null;
+            }
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            if ("designMode" in changedProperties) {
+                this._designMode = changedProperties["designMode"];
+            }
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) {
+            loadDateTimePicker(this);
         }
     }
     customElements.define("krones-sac-testing-sapui5-datatimepicker", TestingDateTimePicker);
 
-    function loadDateTimePicker(){
-        let sWidgetName = "DateTimePicker_1",
-            div         = document.createElement('div');
+    function loadDateTimePicker(that){
+        let that_ = that;
 
-        div.innerHTML = '<?xml version="1.0"?><script id="oView" type="sapui5/xmlview"><mvc:View controllerName="myView.Template" xmlns:mvc="sap.ui.core.mvc" xmlns:l="sap.ui.layout xmlns="sap.m"><DateTimePicker id="DTP1" placeholder="Enter Date"/></mvc:View></script>';
-        shadowRoot.appendChild(div);
-
-        let mapcanvas_divstr = shadowRoot.getElementById('oView' + sWidgetName),
-            aArray = [];
-            aArray.push({
-                'id': sWidgetName,
-                'div': mapcanvas_divstr
-            });
+        let content = document.createElement('div');
+        content.slot = "content";
+        that_.appendChild(content);
 
         sap.ui.getCore().attachInit(function(){
             "use strict";
@@ -54,15 +157,20 @@
                     }
                 });
             });
-            var foundIndex = aArray.findIndex(x => x.id == sWidgetName);
-            var divfinal = aArray[foundIndex].div;
-
-            var oView = sap.ui.xmlview({
-                viewContent: jQuery(divfinal).html(),
-            });
-
-            oView.placeAt(div);
         });
+
+        var oView  = sap.ui.xmlview({
+            viewContent: jQuery(_shadowRoot.getElementById(_id + "_oView")).html(),
+        });
+        oView.placeAt(content);
     }
+
+    function createGuid() {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+            let r = Math.random() * 16 | 0,
+                v = c === "x" ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }  
 
 })();
