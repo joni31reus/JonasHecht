a67df3beee912ab6780ad6b75b99f18a47019daf
joni31reus <hechtjonas@web.de>
2023-11-21 13:20:07 +0100

    Update SAC_DateTimePicker.js

--- SAC_DateTimePicker.js
+++ SAC_DateTimePicker.js
@@ -42,90 +42,6 @@
             _shadowRoot.querySelector("#oView").id = _id + "_oView";
         }
 
-        /*connectedCallback() {
-            try {
-                if (window.commonApp) {
-                    let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0];
-
-                    if (outlineContainer && outlineContainer.getReactProps) {
-                        let parseReactState = state => {
-                            let components = {};
-
-                            let globalState = state.globalState;
-                            let instances = globalState.instances;
-                            let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
-                            let names = app.names;
-
-                            for (let key in names) {
-                                let name = names[key];
-
-                                let obj = JSON.parse(key).pop();
-                                let type = Object.keys(obj)[0];
-                                let id = obj[type];
-
-                                components[id] = {
-                                    type: type,
-                                    name: name
-                                };
-                            }
-
-                            for (let componentId in components) {
-                                let component = components[componentId];
-                            }
-
-                            let metadata = JSON.stringify({
-                                components: components,
-                                vars: app.globalVars
-                            });
-
-                            if (metadata != this.metadata) {
-                                this.metadata = metadata;
-
-                                this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                                    detail: {
-                                        properties: {
-                                            metadata: metadata
-                                        }
-                                    }
-                                }));
-                            }
-                        };
-
-                        let subscribeReactStore = store => {
-                            this._subscription = store.subscribe({
-                                effect: state => {
-                                    parseReactState(state);
-                                    return {
-                                        result: 1
-                                    };
-                                }
-                            });
-                        };
-
-                        let props = outlineContainer.getReactProps();
-                        if (props) {
-                            subscribeReactStore(props.store);
-                        } else {
-                            let oldRenderReactComponent = outlineContainer.renderReactComponent;
-                            outlineContainer.renderReactComponent = e => {
-                                let props = outlineContainer.getReactProps();
-                                subscribeReactStore(props.store);
-
-                                oldRenderReactComponent.call(outlineContainer, e);
-                            }
-                        }
-                    }
-                }
-            } catch (e) {}
-        }
-
-        disconnectedCallback() {
-            if (this._subscription) { 
-                this._subscription();
-                this._subscription = null;
-            }
-        }*/
-
         onCustomWidgetBeforeUpdate(changedProperties) {
             if ("designMode" in changedProperties) {
                 this._designMode = changedProperties["designMode"];
@@ -133,7 +49,7 @@
         }
 
         onCustomWidgetAfterUpdate(changedProperties) {
-            loadDateTimePicker(this);
+            //loadDateTimePicker(this);
         }
 
         _firePropertiesChanged() {
@@ -145,6 +61,10 @@
                     }
                 }
             }));
+        }
+
+        setPlaceholder(sPlaceholderText){
+            console.log(sPlaceholderText);
         }
 
         get dateTime(){
